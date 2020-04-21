fixed computation of emu and test errors

code/calc_emu_error.py
@@ -6,7 +6,8 @@ def main():
     statistic='wp'
     traintag = '_nonolap'
     testtag = '_mean_test0'
-    errtag = '_100hod_test0'
+    #errtag = '_100hod_test0' # for emu
+    errtag = '_hod3_test0'
     tag = '_log_kM32ExpConst2_100hod'
     gptag = traintag + errtag + tag
     acctag = gptag + testtag
@@ -17,20 +18,27 @@ def main():
 
     print("Computing emu error for", statistic, testtag, acctag)
     ptests, ppredicts = load_data(statistic, testtag, acctag, CC_test, HH_test)
-    emu_performance = compute_rmse(ptests, ppredicts)
+    #emu_performance = compute_rmse(ptests, ppredicts)
+    fracerrs = (ptests-ppredicts)/ptests #consider denom here
+    #emu_performance = np.std(fracerrs)
+    cov_perf = covariance(fracerrs, zeromean=True)
+    cov_perf *= 5 #bc using mean of 5 testboxes
 
     res_dir = '../../clust/results_{}/'.format(statistic)
-    testset_error = np.loadtxt(res_dir+"{}_error{}.dat".format(statistic, errtag)) # been calling GP_error
+    errtag = '_hod3_test0' # for cov test
+    cov_test = np.loadtxt(res_dir+"{}_cov{}.dat".format(statistic, errtag))
 
-    #emu_performance^2 = emu_err^2 + testset_err^2
-    emu_err = np.sqrt(emu_performance**2 - testset_error**2)
-    print(emu_err)
-    save_fn = f"../testing_results/{statistic}_emu_error{acctag}.dat"
+    #cov_performance = cov_emu + cov_test
+    cov_emu = cov_perf - cov_test
+    #emu_err = np.sqrt(emu_performance**2 - testset_error**2)
+    print(cov_emu)
+    save_fn = f"../testing_results/{statistic}_emu_cov{acctag}.dat"
     print('Saving to', save_fn)
-    np.savetxt(save_fn, emu_err)
+    np.savetxt(save_fn, cov_emu)
 
+    save_fn_perf = f"../testing_results/{statistic}_emuperf_cov{acctag}.dat"
+    np.savetxt(save_fn_perf, cov_perf)
 
-    
 
 def load_data(statistic, testtag, acctag, CC_test, HH_test):
 
@@ -56,7 +64,23 @@ def load_data(statistic, testtag, acctag, CC_test, HH_test):
             ptests.append(ptest)
             ppredicts.append(ppredict)
     
-    return ptests, ppredicts
+    return np.array(ptests), np.array(ppredicts)
+
+
+
+def covariance(arrs, zeromean=False):
+    N = arrs.shape[0]
+
+    if zeromean:
+        w = arrs
+    else:
+        w = arrs - arrs.mean(0)
+
+    outers = np.array([np.outer(w[n], w[n]) for n in range(N)])
+    covsum = np.sum(outers, axis=0)
+    cov = 1.0/float(N-1.0) * covsum
+    return cov
+
 
 
 def compute_rmse(ys, y_preds):

code/calc_error.py
@@ -8,50 +8,47 @@ def main():
     statistic = 'wp'
 
     testtag = ''
-    errtag = '_100hod_test0'
-    nhods = 100
+    errtag = '_hod3_test0'
 
     res_dir = '../../clust/results_{}/'.format(statistic)
     testing_dir = '../../clust/results_{}/testing_{}{}/'.format(statistic, statistic, testtag)
+
+    # choose a middle-of-the-road hod
+    hod = 3
 
     nbins = 9
     ncosmos = 7
     nboxes = 5
     ntests = 1 #eventually this should be 10
 
-    hods = list(range(nhods))
     cosmos = list(range(ncosmos))
     boxes = list(range(nboxes))
     tests = list(range(ntests)) 
 
-    vals = []
 
     devmeans = []
-    vals_all = []
-    for hod in hods:
-        for cosmo in cosmos:
+    for cosmo in cosmos:
 
-            ys_box = [] #this will contain 5 statistics
-            for box in boxes:
-                
-                # Compute the average over tests for a single box & model
-                ys_test = []
-                for test in tests:
-                    fn = testing_dir+'{}_cosmo_{}_Box_{}_HOD_{}_test_{}.dat'\
-                        .format(statistic, cosmo, box, hod, test)
-                    rad, y = np.loadtxt(fn, delimiter=',', unpack=True)
-                    vals.append(y)
-                    ys_test.append(y)
-                y_box = np.mean(ys_test, axis=0) #mean is our estimate for the statistic of the box with the given model
+        ys_box = [] #this will contain 5 statistics
+        for box in boxes:
+            
+            # Compute the average over tests for a single box & model
+            ys_test = []
+            for test in tests:
+                fn = testing_dir+'{}_cosmo_{}_Box_{}_HOD_{}_test_{}.dat'\
+                    .format(statistic, cosmo, box, hod, test)
+                rad, y = np.loadtxt(fn, delimiter=',', unpack=True)
+                ys_test.append(y)
+            y_box = np.mean(ys_test, axis=0) #mean is our estimate for the statistic of the box with the given model
 
-                ys_box.append(y_box)
+            ys_box.append(y_box)
 
-            #The mean of the 5 boxes, for a given model (cosmo & HOD)
-            y_mean = np.mean(ys_box, axis=0) 
+        #The mean of the 5 boxes, for a given model (cosmo & HOD)
+        y_mean = np.mean(ys_box, axis=0) 
 
-            for y in ys_box:
-                devmean = (y-y_mean)/y_mean
-                devmeans.append(devmean)
+        for y in ys_box:
+            devmean = (y-y_mean)/y_mean
+            devmeans.append(devmean)
             
     #compute covariance assuming the mean is zero, as that is the expectation value (should be unbiased)
     devmeans = np.array(devmeans)
@@ -63,16 +60,14 @@ def main():
     np.savetxt(res_dir+"{}_error{}.dat".format(statistic, errtag), err)
     print("err:", err)
     
-    #p16 = np.percentile(devmeans, 16, axis=0)
-    #p84 = np.percentile(devmeans, 84, axis=0)
+    p16 = np.percentile(devmeans, 16, axis=0)
+    p84 = np.percentile(devmeans, 84, axis=0)
     #std_obs = np.std(vals_all, axis=0)
     #save to both the directory and the mean, same error for both
     # 
-    # np.savetxt(res_dir+"{}_p16{}.dat".format(statistic, errtag), p16)
-    # np.savetxt(res_dir+"{}_p84{}.dat".format(statistic, errtag), p84)
+    np.savetxt(res_dir+"{}_p16{}.dat".format(statistic, errtag), p16)
+    np.savetxt(res_dir+"{}_p84{}.dat".format(statistic, errtag), p84)
     # np.savetxt(res_dir+"{}_std{}.dat".format(statistic, errtag), std_obs)
-    #wps_box_avg /= len(hods)
-    #        wps_grid[cosmo][box] += wps_box_avg
 
 
 def covariance(arrs, zeromean=False):
@@ -81,7 +76,7 @@ def covariance(arrs, zeromean=False):
     if zeromean:
         w = arrs
     else:
-        w = arrs - arrs.mean(0)[:, np.newaxis]
+        w = arrs - arrs.mean(0)
 
     outers = np.array([np.outer(w[n], w[n]) for n in range(N)])
     covsum = np.sum(outers, axis=0)